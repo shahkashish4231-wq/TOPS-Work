890b7b96a9f1c47cc91661fe570a97819f400b5e
shahkashish4231-wq <shahkashish4231@gmail.com>
2026-08-26 11:20:11 +0530

    Create Task1.py

---
+++ Assignments/Python/Task1.py
@@ -0,0 +1,17 @@
+2.Create a new Jupyter Notebook file named analytics_intro.ipynb and write a cell that prints your name and 
+your favorite app (like Instagram or Zomato) using the print() function.
+
+
+print("My name is Kashish Shah")
+print("My favourite app is Instagram")
+
+3.Open VS Code or any code editor, create a Python script file called hello_app.py, and write code to 
+print 'Welcome to Data Analytics with Python!'. Then, run this script from the terminal.
+
+print("Welcome to Data Analytics with Python!")
+
+4. In Jupyter Notebook, add a new cell and write Python code to print the current year using the datetime module.<br><br><em><strong>
+Hint:</strong> Import datetime and use datetime.datetime.now().year.</em>
+
+import datetime
+print(datetime.datetime.now().year)
